5935a359742009e0d73e3adaef8ac8fcb8919a7c
Useless8000B <adrielluiz73@gmail.com>
2026-04-24 15:01:37 -0300

    Add method to ExamRepository for retrieving exams by Firebase UUID

--- src/main/java/com/useless/health_api/repositories/ExamRepository.java
+++ src/main/java/com/useless/health_api/repositories/ExamRepository.java
@@ -1,7 +1,11 @@
 package com.useless.health_api.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.useless.health_api.models.ExamModel;
 
-public interface ExamRepository extends JpaRepository <ExamModel, Long>{}
+public interface ExamRepository extends JpaRepository <ExamModel, Long>{
+	List<ExamModel> findByFirebaseUuid(String firebaseUuid);
+}
