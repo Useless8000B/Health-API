93dc3b7687d37652ba3d195ec79eaf6595ecdc1d
Useless8000B <adrielluiz73@gmail.com>
2026-04-26 10:29:10 -0300

    Added global exception handler

---
+++ src/main/java/com/useless/health_api/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.useless.health_api.exceptions;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.useless.health_api.dto.ErrorResponse;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorResponse> handleGenericExceptions(Exception e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+		return buildErrorResponse(status, e, request);
+	}
+
+	@ExceptionHandler(NotFoundException.class)
+	public ResponseEntity<ErrorResponse> handleNotFound(NotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		return buildErrorResponse(status, e, request);
+	}
+
+	private ResponseEntity<ErrorResponse> buildErrorResponse(
+		HttpStatus status,
+		Exception e,
+		HttpServletRequest request
+	) {
+		ErrorResponse error = new ErrorResponse(
+			LocalDateTime.now(),
+			status.value(),
+			status.getReasonPhrase(),
+			e.getMessage(),
+			request.getRequestURI()
+		);
+
+		return ResponseEntity.status(status).body(error);
+	}
+}
