fc5e6158c32f74cd1484aacd1824b0100f898fb1
Useless8000B <adrielluiz73@gmail.com>
2026-04-26 10:29:34 -0300

    Refactored exam controller to include error handling

--- src/main/java/com/useless/health_api/controllers/ExamController.java
+++ src/main/java/com/useless/health_api/controllers/ExamController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.useless.health_api.exceptions.NotFoundException;
 import com.useless.health_api.models.ExamModel;
 import com.useless.health_api.repositories.ExamRepository;
 
@@ -25,13 +26,26 @@ public class ExamController {
 	public ResponseEntity<List<ExamModel>> getExams(HttpServletRequest request) {
 		String uid = (String) request.getAttribute("uid");
 
+		if (uid == null || uid.isEmpty()) {
+			throw new RuntimeException("User not authenticated properly");
+		}
+
 		List<ExamModel> exams = examRepository.findByFirebaseUuid(uid);
+
+		if (exams.isEmpty()) {
+			throw new NotFoundException("No medical exams for this user");
+		}
+
 		return ResponseEntity.ok(exams);
 	}
 
 	@PostMapping
 	public ExamModel createExam(@RequestBody ExamModel exam, HttpServletRequest request) {
 		String uid = (String) request.getAttribute("uid");
+		
+		if (uid == null || uid.isEmpty()) {
+			throw new RuntimeException("User not authenticated properly");
+		}
 
 		exam.setFirebaseUuid(uid);
 
